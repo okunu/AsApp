b21d5ca3039010894856e30ed7497e3e0743582a
okunu <okunu@qq.com>
2017-10-21 14:30:32 +0800

    添加高斯模糊注释

--- app/src/main/jni/GaussBlur.c
+++ app/src/main/jni/GaussBlur.c
@@ -6,6 +6,8 @@
 #include <jni.h>
 #include <android/log.h>
 #include<malloc.h>
+#include <math.h>
+#include <string.h>
 
 #define LOG_TAG "okunu"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG, __VA_ARGS__)
@@ -15,54 +17,69 @@
 #define MIN(a, b) ((a)<(b)?(a):(b))
 
 static void gaussBlur(int *pix, int w, int h, int radius) {
+    //分别计算正态分布各参数，sigma与高斯模糊半径有关系，2.57既是1除以根号2 PI得来
     float sigma = 1.0 * radius / 2.57;
     float deno = 1.0 / sigma * sqrt(2.0 * PI);
     float nume = -1.0 / (2.0 * sigma * sigma);
 
+    //计算正态分布下的权重值
     float *gaussMatrix = (float *) malloc(sizeof(float) * (radius + radius + 1));
     float gaussSum = 0.0;
     for (int i = 0, x = -radius; x <= radius; ++x, ++i) {
         float g = deno * exp(1.0 * nume * x * x);
+        // 注意 x 与 i 的对应关系，x 是相对距离
         gaussMatrix[i] = g;
         gaussSum += g;
     }
+
+    //因为没有计算所有的周围点，权重值和不为1，所以需要归一化。
     int len = radius + radius + 1;
     for (int i = 0; i < len; ++i) {
         gaussMatrix[i] /= gaussSum;
     }
-
+    //申请两个数组，分别用于存放一行或一列的像素值
     int *rowData = (int *) malloc(w * sizeof(int));
     int *listData = (int *) malloc(h * sizeof(int));
-
+    //开始x轴方向模糊
     for (int y = 0; y < h; ++y) {
+        //取一行像素数据，注意像素总数组的访问方式是  x + y * w
         memcpy(rowData, pix + y * w, sizeof(int) * w);
         for (int x = 0; x < w; ++x) {
             float r = 0, g = 0, b = 0;
             gaussSum = 0;
+            //以当前坐标点 x、y 为中心，查看前后一个模糊半径的周围点，根据正态分布
+            //重新计算像素点的颜色值
             for (int i = -radius; i <= radius; ++i) {
+                // k 表示周围点的真实坐标
                 int k = x + i;
+                // 边界上的像素点，它的周围点只有正常的一半，所以要保证 k 的取值范围
                 if (k >= 0 && k <= w) {
+                    // 取到周围点的像素，并根据 argb 的排列方式，计算 r、g、b分量
                     int color = rowData[k];
                     int cr = (color & 0x00ff0000) >> 16;
                     int cg = (color & 0x0000ff00) >> 8;
                     int cb = (color & 0x000000ff);
-
+                    //真实点坐标为 k，与它对应的权重数组下标是 i + radius
+                    //前文中计算正态分布权重时已经说明相关的对应关系。
+                    //根据正态分布的权重关系，计算中心点的 r g b各分量
                     int index = i + radius;
                     r += cr * gaussMatrix[index];
                     g += cg * gaussMatrix[index];
                     b += cb * gaussMatrix[index];
-
                     gaussSum += gaussMatrix[index];
-
                 }
             }
+            //因为边界点的存在，gaussSum值不一定为1，所以需要除以gaussSum，归一化。
             int cr = (int) (r / gaussSum);
             int cg = (int) (g / gaussSum);
             int cb = (int) (b / gaussSum);
+            //根据权重值与各周围点像素相乘之和，得到新的中间点像素。
             pix[y * w + x] = cr << 16 | cg << 8 | cb | 0xff000000;
         }
     }
+    //y轴方向高斯模糊
     for (int x = 0; x < w; ++x) {
+        //复制一列颜色数据到数组中
         for (int y = 0; y < h; ++y) {
             listData[y] = pix[y * w + x];
         }
